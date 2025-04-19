e8c7c1b00dc8458fcfc1492d167a238c998f3514
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2025-04-19 05:14:55 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: [
+  content: [ 
     "./pages/**/*.{js,jsx}",
     "./components/**/*.{js,jsx}",
     "./app/**/*.{js,jsx}",
